09594c2f943c06cd96e74fd5af048981be091944
iamlokesh4298 <125215919+iamlokesh4298@users.noreply.github.com>
2023-02-23 06:06:41 +0530

    Add files via upload

---
+++ prog8.py
@@ -0,0 +1,8 @@
+#write apython program to sum all the numbers in the list[8,2,3,0,7]
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+        return total
+        print(sum(8,2,3,0,7))
+        
